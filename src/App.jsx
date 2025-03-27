b5212f519ec40e3915887e59e2b6915b648cb7f0
HALO\gangs <nprasad2077@gmail.com>
2025-03-27 17:43:05 -0500

    tailwind init

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p class='text-9xl'>Vite + React</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
